d76023413287aa34eeca8bf47442506c38e64a9e
David Zambrano <davazamb10@outlook.com>
2016-12-23 19:21:41 -0400

    Restrincion de acceso a opciones de menu

--- src/CorePersons/Controllers/AccountController.cs
+++ src/CorePersons/Controllers/AccountController.cs
@@ -129,7 +129,8 @@ namespace CorePersons.Controllers
                     //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction(nameof(AccountController.Register), "Account");
+                    //return RedirectToLocal(returnUrl);
                 }
                 AddErrors(result);
             }
